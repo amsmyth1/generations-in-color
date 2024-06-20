c0ba2d440dc7c024ca06caa8b0d5ad3f03264aaa
Alexa Morales <alexamsmyth@gmail.com>
2024-06-20 10:06:29 -0400

    Create more form fill ins for the user. Create a prompt from the users input

--- image_generator/generator/forms.py
+++ image_generator/generator/forms.py
@@ -1,4 +1,10 @@
 from django import forms
 
-class ImageGenerationForm(forms.Form):
-    prompt = forms.CharField(label='Prompt', max_length=255)
+# class ImageGenerationForm(forms.Form):
+#     prompt = forms.CharField(label='Prompt', max_length=255)
+
+class StoryForm(forms.Form):
+    family_member_name = forms.CharField(label='Family Member Name', max_length=100)
+    relation = forms.CharField(label='Relation to Main Character', max_length=100)
+    year = forms.IntegerField(label='Year of the Picture')
+    description = forms.CharField(label='Description of the Story', widget=forms.Textarea)
--- image_generator/generator/views.py
+++ image_generator/generator/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.conf import settings
 import openai
-from .forms import ImageGenerationForm
+from .forms import StoryForm
 from dotenv import load_dotenv
 
 openai.api_key = settings.OPENAI_API_KEY
 
+def generate_coloring_book_prompt(family_member_name, relation, year, description):
+    coloring_book_prompt = f"Generate a picture in the style of a simple clean lined coloring book. Using black lines with a white background. The photo should be in the year of {year}. Depicting {description}"
+    prompt = coloring_book_prompt
+    return prompt
+
 def generate_image(request):
     if request.method == 'POST':
-        form = ImageGenerationForm(request.POST)
+        form = StoryForm(request.POST)
         if form.is_valid():
-            prompt = form.cleaned_data['prompt']
+            family_member_name = form.cleaned_data['family_member_name']
+            relation = form.cleaned_data['relation']
+            year = form.cleaned_data['year']
+            description = form.cleaned_data['description']
+            prompt = generate_coloring_book_prompt(family_member_name, relation, year, description)
+            print(f"*********************************************Prompt: {prompt}")
             response = openai.images.generate(
                 model="dall-e-3",
                 prompt=prompt,
@@ -21,5 +31,5 @@ def generate_image(request):
             image_url = response.data[0].url
             return render(request, 'generator/result.html', {'image_url': image_url})
     else:
-        form = ImageGenerationForm()
+        form = StoryForm()
     return render(request, 'generator/form.html', {'form': form})
